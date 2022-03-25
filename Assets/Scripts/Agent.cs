e344f474121bb42570b6a64133da15fb2b7ff00c
LuiMoiPer <luimoiper@outlook.com>
2022-03-24 22:53:15 -0700

    Added agent class responsibilities.

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,9 @@
+public class Agent {
+    /* Responsibilities
+    Build roads
+    Build villages
+    Upgrade villages to towns
+    Offer trades to other agents
+    Accept trades from other agents
+    */
+}
